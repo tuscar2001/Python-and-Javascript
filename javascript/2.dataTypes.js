71c37fc7fe8adfcd888371e92eb3addbca2a7850
Oscar Adimi <tuscar2001@gmail.com>
2023-09-04 09:37:16 -0700

    initial commit

---
+++ javascript/2.dataTypes.js
@@ -0,0 +1,13 @@
+console.log("*********This is a string**************");
+console.log("John Doe");
+console.log("John Doe is of type: ",typeof("John Doe"));
+
+console.log("*********These are numbers**************");
+console.log(1);
+console.log(1+2);
+console.log("1 is of type: ",typeof(1));
+
+console.log("**********This are booleans**********");
+console.log(true);
+console.log(false);
+console.log("True is of type: ", typeof(true));
---
+++ python/2.dataTypes.py
@@ -0,0 +1,13 @@
+print ("*********This is a string**************")
+print("John Doe")
+print ("John Doe is of type: ",type("John Doe"))
+
+print ("*********These are numbers**************")
+print (1)
+print (1+2)
+print ("1 is of type: ",type(1))
+
+print("This are booleans")
+print(True)
+print(False)
+print("True is of type: ", type(True))
