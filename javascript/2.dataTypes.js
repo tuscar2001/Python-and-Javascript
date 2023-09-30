303638285167ed1889615b68ce1707f54131b7f6
Oscar Adimi <tuscar2001@gmail.com>
2023-09-30 08:10:07 -0700

    checking commits

--- javascript/2.dataTypes.js
+++ javascript/2.dataTypes.js
@@ -1,13 +1,15 @@
-console.log("*********This is a string**************");
+console.log("****************this is a string****************");
 console.log("John Doe");
-console.log("John Doe is of type: ",typeof("John Doe"));
+console.log("John Doe is of type: ", typeof("John Doe"));
 
-console.log("*********These are numbers**************");
+
+console.log("****************these are numbers****************");
 console.log(1);
 console.log(1+2);
-console.log("1 is of type: ",typeof(1));
+console.log("1 is of type: ", typeof(1));
 
-console.log("**********This are booleans**********");
+
+console.log("****************these are booleans****************");
 console.log(true);
 console.log(false);
-console.log("True is of type: ", typeof(true));
+console.log("true is of type", typeof(true));
--- python/2.dataTypes.py
+++ python/2.dataTypes.py
@@ -1,13 +1,15 @@
-print ("*********This is a string**************")
-print("John Doe")
-print ("John Doe is of type: ",type("John Doe"))
+print("******************This is a string******************")
+print ("John Doe")
+print("John Doe is of type: ",type("John Doe"))
 
-print ("*********These are numbers**************")
+
+print("******************This is a number******************")
 print (1)
-print (1+2)
-print ("1 is of type: ",type(1))
+print(1+2)
+print("1 of type: ",type(1))
 
-print("This are booleans")
-print(True)
+
+print("******************This is a boolean******************")
+print (True)
 print(False)
-print("True is of type: ", type(True))
+print("True of type: ",type(True))
